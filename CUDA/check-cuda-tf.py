ec88152d57c7daa549ff2cee3740345a5d4760db
kurekagura <kurekagura@gmail.com>
2022-11-14 20:45:38 +0900

    CUDAチェックpyの修正。cu110で動作確認済み。

--- CUDA/check-cuda-tf.py
+++ CUDA/check-cuda-tf.py
@@ -1,3 +1,5 @@
+#CUDA 11.0
+#pip install tensorflow=2.40
 import sys
 import tensorflow as tf
 
@@ -8,9 +10,10 @@ print(f"tf.__version__ => {tf.__version__}")
 #is_gpu = tf.test.is_gpu_available()
 #print(f"tf.test.is_gpu_available() => {is_gpu}")
 
-print("tf.config.list_physical_devices('GPU') => ")
 gpu_devices = tf.config.list_physical_devices('GPU')
+print(f"tf.config.list_physical_devices('GPU') => \n{gpu_devices}")
 
-if gpu_devices:
-    print("tf.python.client.device_lib.list_local_devices() => ")
-    tf.python.client.device_lib.list_local_devices()
+if len(gpu_devices)>0:
+    from tensorflow.python.client import device_lib
+    local_devices = device_lib.list_local_devices()
+    print(f"device_lib.list_local_devices() => \n{local_devices}")
--- CUDA/check-cuda-torch.py
+++ CUDA/check-cuda-torch.py
@@ -1,3 +1,5 @@
+#CUDA 11.0
+#pip install torch==1.7.1+cu110 torchvision==0.8.2+cu110 torchaudio==0.7.2 -f https://download.pytorch.org/whl/torch_stable.html
 import sys
 import torch
 

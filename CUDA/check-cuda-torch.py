6d7410c2704768ccf83f7388204790e36c0b7c09
kurekagura <kurekagura@gmail.com>
2022-11-21 23:01:47 +0900

    torchからcudaバージョンの取得

--- CUDA/check-cuda-torch.py
+++ CUDA/check-cuda-torch.py
@@ -1,3 +1,4 @@
+#curl https://raw.githubusercontent.com/kurekagura/SetupGuide/main/CUDA/check-cuda-torch.py -O
 #CUDA 11.0
 #pip install torch==1.7.1+cu110 torchvision==0.8.2+cu110 torchaudio==0.7.2 -f https://download.pytorch.org/whl/torch_stable.html
 #CUDA 11.7
@@ -12,11 +13,12 @@ print(f"torch.cuda.is_available() => {torch.cuda.is_available()}")
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 if device == "cuda":
+    print(f"torch.version.cuda => {torch.version.cuda}")
+    print(f"torch.backends.cudnn.version() => {torch.backends.cudnn.version()}")
+
     print(f"torch.cuda.device_count() => {torch.cuda.device_count()}")
     print(f"torch.cuda.current_device() => {torch.cuda.current_device()}")
 
     torch_device = torch.device(f"cuda:{torch.cuda.current_device()}")
     print(f"torch.cuda.get_device_name({torch_device}) => {torch.cuda.get_device_name(torch_device)}")
     print(f"torch.cuda.get_device_capability(torch_device) => {torch.cuda.get_device_capability(torch_device)}")
-
-    print(f"torch.backends.cudnn.version() => {torch.backends.cudnn.version()}")

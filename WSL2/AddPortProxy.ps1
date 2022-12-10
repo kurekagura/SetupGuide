3bba3ba8f5ddd1335128fae5dbaf25a01b0dcf7f
kurekagura <26186258+kurekagura@users.noreply.github.com>
2022-12-11 02:21:26 +0900

    Updated ps1.

--- WSL2/AddPortProxy.ps1
+++ WSL2/AddPortProxy.ps1
@@ -10,6 +10,11 @@ netsh.exe interface portproxy add v4tov4 listenaddress=$HOSTIP listenport=$Port 
 #If InterfaceIndex is invalid, netsh.exe just shows help.
 
 netsh.exe interface portproxy show v4tov4
-Write-Host PortProxy to $Distro`n
+
+if([string]::IsNullOrEmpty($HOSTIP)){
+    $HOSTIP = "<SRC IP>"
+}
 Write-Host "If you want to delete, use the following command with run as Administrator."
-Write-Host "netsh.exe interface portproxy delete v4tov4 listenaddress=<src ip> listenport=<src port>`n"
+Write-Host "> netsh.exe interface portproxy delete v4tov4 listenaddress=$HOSTIP listenport=$PORT"
+Write-Host "and you can check it."
+Write-Host "> netsh.exe interface portproxy show v4tov4`n"

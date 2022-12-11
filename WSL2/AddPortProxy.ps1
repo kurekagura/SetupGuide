f4bc09a1a4d21a31c992414a0fbceecfd9e2cec7
kurekagura <26186258+kurekagura@users.noreply.github.com>
2022-12-11 22:10:20 +0900

    Invoke-Expressionを使わないとうまくいかないよう（成功しているように見えるが）。

--- WSL2/AddPortProxy.ps1
+++ WSL2/AddPortProxy.ps1
@@ -1,11 +1,11 @@
-param(
+﻿param(
     [parameter(mandatory=$true)][int32]$InterfaceIndex,
     [int32]$Port=873,
     [ValidateSet("Ubuntu-20.04","Ubuntu-22.04")][String]$Distro="Ubuntu-20.04")
 
 $HOSTIP = (Get-NetIPConfiguration|Where-Object {$_.InterfaceIndex -eq $InterfaceIndex}|Select-Object -ExpandProperty IPv4Address).IPAddress
 $UBUNTUIP = wsl -d $Distro -e hostname -I
-netsh.exe interface portproxy add v4tov4 listenaddress=$HOSTIP listenport=$Port connectaddress=$UBUNTUIP connectport=$Port
+Invoke-Expression "netsh.exe interface portproxy add v4tov4 listenaddress=$HOSTIP listenport=$Port connectaddress=$UBUNTUIP connectport=$Port"
 #If($LastExitCode -gt 0) {throw}
 #If InterfaceIndex is invalid, netsh.exe just shows help.
 
